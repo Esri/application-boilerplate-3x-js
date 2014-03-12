7cb4e985ad83497cde0165f43bb516eccaaa5224
Kelly Hutchins <khutchins@esri.com>
2014-03-12 13:48:03 -0700

    Update defaults.js

--- application_boilerplate/config/defaults.js
+++ application_boilerplate/config/defaults.js
@@ -2,7 +2,7 @@ define({
     //Default configuration settings for the application. This is where you'll define things like a bing maps key,
     //default web map, default app color theme and more. These values can be overwritten by template configuration settings and url parameters.
     "appid": "",
-    "webmap": "de5ae0c2040c49d38e9ea0637454ac73",
+    "webmap": "24e01ef45d40423f95300ad2abc5038a",
     "oauthappid": null, //"AFTKRmv16wj14N3z",
     //Group templates must support a group url parameter. This will contain the id of the group. 
     //group: "",
