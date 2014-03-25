cb88cffed0d2b2d464abb753dd2b887fba4a997d
MikeTschudi <mtschudi@esri.com>
2014-03-25 13:11:53 -0700

    JSLinted

--- application_boilerplate/config/defaults.js
+++ application_boilerplate/config/defaults.js
@@ -1,34 +1,35 @@
+/*global define,location */
 define({
     //Default configuration settings for the application. This is where you'll define things like a bing maps key,
     //default web map, default app color theme and more. These values can be overwritten by template configuration settings and url parameters.
     "appid": "",
     "webmap": "24e01ef45d40423f95300ad2abc5038a",
     "oauthappid": null, //"AFTKRmv16wj14N3z",
-    //Group templates must support a group url parameter. This will contain the id of the group. 
+    //Group templates must support a group url parameter. This will contain the id of the group.
     //group: "",
     //Enter the url to the proxy if needed by the application. See the 'Using the proxy page' help topic for details
     //http://developers.arcgis.com/en/javascript/jshelp/ags_proxy.html
     "proxyurl": "",
     //Example of a template specific property. If your template had several color schemes
     //you could define the default here and setup configuration settings to allow users to choose a different
-    //color theme.  
+    //color theme.
     "theme": "blue",
     "bingmapskey": "", //Enter the url to your organizations bing maps key if you want to use bing basemaps
     //Defaults to arcgis.com. Set this value to your portal or organization host name.
     "sharinghost": location.protocol + "//" + "www.arcgis.com",
-    //When true the template will query arcgis.com for default settings for helper services, units etc. If you 
+    //When true the template will query arcgis.com for default settings for helper services, units etc. If you
     //want to use custom settings for units or any of the helper services set queryForOrg to false then enter
-    //default values for any items you need using the helper services and units properties. 
+    //default values for any items you need using the helper services and units properties.
     "queryForOrg": true,
-    //If you need localization set the supportsLocalization value to true to get the localized strings 
-    //from the javascript/nls/resource files. 
+    //If you need localization set the supportsLocalization value to true to get the localized strings
+    //from the javascript/nls/resource files.
     //Note that we've included a placeholder nls folder and a resource file with one error string
     //to show how to setup the strings file.
     "localize": true,
     "units": null,
-    //This option demonstrates how to handle additional custom url parameters. For example 
-    //if you want users to be able to specify lat/lon coordinates that define the map's center or 
-    //specify an alternate basemap via a url parameter. 
+    //This option demonstrates how to handle additional custom url parameters. For example
+    //if you want users to be able to specify lat/lon coordinates that define the map's center or
+    //specify an alternate basemap via a url parameter.
     "urlItems": [
         "theme" // example param. ?theme=<my theme>
     ],
@@ -44,6 +45,6 @@ define({
         },
         "geocode": [{
             "url": null
-       }]
+        }]
     }
 });
--- application_boilerplate/js/template.js
+++ application_boilerplate/js/template.js
@@ -1,3 +1,5 @@
+/*global define,document,location,require */
+/*jslint sloppy:true,nomen:true,plusplus:true */
 define([
     "dojo/Evented",
     "dojo/_base/declare",
@@ -41,7 +43,7 @@ define([
         appConfig: {},
         customUrlConfig: {},
         constructor: function () {
-            // config will contain application and user defined info for the application such as i18n strings, 
+            // config will contain application and user defined info for the application such as i18n strings,
             // the web map id and application id, any url parameters and any application specific configuration
             // information.
             this.config = defaults;
@@ -60,22 +62,24 @@ define([
         // Get URL parameters and set application defaults needed to query arcgis.com for
         // an application and to see if the app is running in Portal or an Org
         _init: function () {
-            var deferred = new Deferred();
-            // Set the web map, group and appid if they exist but ignore other url params. 
+            var deferred, paramItems, mixinParams;
+
+            deferred = new Deferred();
+            // Set the web map, group and appid if they exist but ignore other url params.
             // Additional url parameters may be defined by the application but they need to be mixed in
             // to the config object after we retrieve the application configuration info. As an example,
             // we'll mix in some commonly used url parameters in the _queryUrlParams function after
             // the application configuration has been applied so that the url parameters overwrite any
-            // configured settings. It's up to the application developer to update the application to take 
-            // advantage of these parameters. 
-            var paramItems = ["webmap", "appid", "group", "oauthappid"];
-            var mixinParams = this._createUrlParamsObject(paramItems);
+            // configured settings. It's up to the application developer to update the application to take
+            // advantage of these parameters.
+            paramItems = ["webmap", "appid", "group", "oauthappid"];
+            mixinParams = this._createUrlParamsObject(paramItems);
             // config defaults <- standard url params
             // we need the webmap, appid, group and oauthappid to query for the data
             lang.mixin(this.config, mixinParams);
-            // Define the sharing url and other default values like the proxy. 
+            // Define the sharing url and other default values like the proxy.
             // The sharing url defines where to search for the web map and application content. The
-            // default value is arcgis.com. 
+            // default value is arcgis.com.
             this._initializeApplication();
             // execute these async
             all({
@@ -98,7 +102,7 @@ define([
                     // mix in all the settings we got!
                     // defaults <- organization <- application id config <- custom url params
                     lang.mixin(this.config, this.orgConfig, this.appConfig, this.customUrlConfig);
-                    // Set the geometry helper service to be the app default.  
+                    // Set the geometry helper service to be the app default.
                     if (this.config.helperServices && this.config.helperServices.geometry && this.config.helperServices.geometry.url) {
                         esriConfig.defaults.geometryService = new GeometryService(this.config.helperServices.geometry.url);
                     }
@@ -113,19 +117,20 @@ define([
             return deferred.promise;
         },
         _createUrlParamsObject: function (items) {
-            // retrieve url parameters. Templates all use url parameters to determine which arcgis.com 
-            // resource to work with. 
+            var urlObject, obj = {}, i;
+
+            // retrieve url parameters. Templates all use url parameters to determine which arcgis.com
+            // resource to work with.
             // Map templates use the webmap param to define the webmap to display
-            // Group templates use the group param to provide the id of the group to display. 
-            // appid is the id of the application based on the template. We use this 
-            // id to retrieve application specific configuration information. The configuration 
-            // information will contain the values the  user selected on the template configuration 
-            // panel.  
-            var urlObject = urlUtils.urlToObject(document.location.href);
+            // Group templates use the group param to provide the id of the group to display.
+            // appid is the id of the application based on the template. We use this
+            // id to retrieve application specific configuration information. The configuration
+            // information will contain the values the  user selected on the template configuration
+            // panel.
+            urlObject = urlUtils.urlToObject(document.location.href);
             urlObject.query = urlObject.query || {};
-            var obj = {};
             if (urlObject.query && items && items.length) {
-                for (var i = 0; i < items.length; i++) {
+                for (i = 0; i < items.length; i++) {
                     if (urlObject.query[items[i]]) {
                         obj[items[i]] = urlObject.query[items[i]];
                     }
@@ -134,17 +139,19 @@ define([
             return obj;
         },
         _initializeApplication: function () {
+            var appLocation, instance;
+
             // Check to see if the app is hosted or a portal. If the app is hosted or a portal set the
-            // sharing url and the proxy. Otherwise use the sharing url set it to arcgis.com. 
-            // We know app is hosted (or portal) if it has /apps/ or /home/ in the url. 
-            var appLocation = location.pathname.indexOf("/apps/");
+            // sharing url and the proxy. Otherwise use the sharing url set it to arcgis.com.
+            // We know app is hosted (or portal) if it has /apps/ or /home/ in the url.
+            appLocation = location.pathname.indexOf("/apps/");
             if (appLocation === -1) {
                 appLocation = location.pathname.indexOf("/home/");
             }
-            // app is hosted and no sharing url is defined so let's figure it out. 
+            // app is hosted and no sharing url is defined so let's figure it out.
             if (appLocation !== -1) {
                 // hosted or portal
-                var instance = location.pathname.substr(0, appLocation); //get the portal instance name
+                instance = location.pathname.substr(0, appLocation); //get the portal instance name
                 this.config.sharinghost = location.protocol + "//" + location.host + instance;
                 this.config.proxyurl = location.protocol + "//" + location.host + instance + "/sharing/proxy";
             } else {
@@ -155,16 +162,18 @@ define([
                 }
             }
             arcgisUtils.arcgisUrl = this.config.sharinghost + "/sharing/rest/content/items";
-            // Define the proxy url for the app 
+            // Define the proxy url for the app
             if (this.config.proxyurl) {
                 esriConfig.defaults.io.proxyUrl = this.config.proxyurl;
                 esriConfig.defaults.io.alwaysUseProxy = false;
             }
         },
         _checkSignIn: function () {
-            var deferred = new Deferred();
+            var deferred, signedIn;
+
+            deferred = new Deferred();
             // check sign-in status
-            var signedIn = IdentityManager.checkSignInStatus(this.config.sharinghost + "/sharing");
+            signedIn = IdentityManager.checkSignInStatus(this.config.sharinghost + "/sharing");
             // resolve regardless of signed in or not.
             signedIn.promise.always(function () {
                 deferred.resolve(true);
@@ -179,31 +188,32 @@ define([
             });
         },
         _getLocalization: function () {
-            var deferred = new Deferred();
+            var deferred, dirNode, classes, rtlClasses;
+
+            deferred = new Deferred();
             if (this.config.localize) {
                 require(["dojo/i18n!application/nls/resources"], lang.hitch(this, function (appBundle) {
-                    // Get the localization strings for the template and store in an i18n variable. Also determine if the 
-                    // application is in a right-to-left language like Arabic or Hebrew. 
+                    // Get the localization strings for the template and store in an i18n variable. Also determine if the
+                    // application is in a right-to-left language like Arabic or Hebrew.
                     this.config.i18n = appBundle || {};
                     // Bi-directional language support added to support right-to-left languages like Arabic and Hebrew
-                    // Note: The map must stay ltr  
+                    // Note: The map must stay ltr
                     this.config.i18n.direction = "ltr";
                     array.some(["ar", "he"], lang.hitch(this, function (l) {
                         if (kernel.locale.indexOf(l) !== -1) {
                             this.config.i18n.direction = "rtl";
                             return true;
-                        } else {
-                            return false;
                         }
+                        return false;
                     }));
                     // add a dir attribute to the html tag. Then you can add special css classes for rtl languages
-                    var dirNode = document.getElementsByTagName("html")[0];
-                    var classes = dirNode.className;
+                    dirNode = document.getElementsByTagName("html")[0];
+                    classes = dirNode.className;
                     if (this.config.i18n.direction === "rtl") {
-                        // need to add support for dj_rtl. 
-                        // if the dir node is set when the app loads dojo will handle. 
+                        // need to add support for dj_rtl.
+                        // if the dir node is set when the app loads dojo will handle.
                         dirNode.setAttribute("dir", "rtl");
-                        var rtlClasses = " esriRTL dj_rtl dijitRtl " + classes.replace(/ /g, "-rtl ");
+                        rtlClasses = " esriRTL dj_rtl dijitRtl " + classes.replace(/ /g, "-rtl ");
                         dirNode.className = lang.trim(classes + rtlClasses);
                     } else {
                         dirNode.setAttribute("dir", "ltr");
@@ -217,14 +227,16 @@ define([
             return deferred.promise;
         },
         _queryDisplayItem: function () {
+            var deferred, itemId, error;
+
             // Get details about the specified web map or group. If the group or web map is not shared publicly users will
             // be prompted to log-in by the Identity Manager.
-            var deferred = new Deferred();
+            deferred = new Deferred();
             if (this.config.webmap || this.config.group) {
-                var itemId = this.config.webmap || this.config.group;
+                itemId = this.config.webmap || this.config.group;
                 arcgisUtils.getItem(itemId).then(lang.hitch(this, function (itemInfo) {
-                    // ArcGIS.com allows you to set an application extent on the application item. Overwrite the 
-                    // existing web map extent with the application item extent when set. 
+                    // ArcGIS.com allows you to set an application extent on the application item. Overwrite the
+                    // existing web map extent with the application item extent when set.
                     if (this.config.appid && this.config.application_extent.length > 0 && itemInfo.item.extent) {
                         itemInfo.item.extent = [
                             [
@@ -237,7 +249,7 @@ define([
                             ]
                         ];
                     }
-                    // Set the itemInfo config option. This can be used when calling createMap instead of the webmap or group id 
+                    // Set the itemInfo config option. This can be used when calling createMap instead of the webmap or group id
                     this.config.itemInfo = itemInfo;
                     deferred.resolve(true);
                 }), function (error) {
@@ -247,7 +259,7 @@ define([
                     deferred.reject(error);
                 });
             } else {
-                var error = new Error("ApplicationBoilerplate:: webmap or group undefined.");
+                error = new Error("ApplicationBoilerplate:: webmap or group undefined.");
                 deferred.reject(error);
             }
             return deferred.promise;
@@ -260,12 +272,12 @@ define([
             if (this.config.appid) {
                 arcgisUtils.getItem(this.config.appid).then(lang.hitch(this, function (response) {
                     if (response.item && response.itemData && response.itemData.values) {
-                        // get app config values - we'll merge them with config later. 
+                        // get app config values - we'll merge them with config later.
                         this.appConfig = response.itemData.values;
                         // save response
                         this.appResponse = response;
                         // Get the web map from the app values. But if there's a web url
-                        // parameter don't overwrite with the app value. 
+                        // parameter don't overwrite with the app value.
                         var webmapParam = this._createUrlParamsObject(["webmap"]);
                         if (!esriLang.isDefined(webmapParam.webmap) && response.itemData.values.webmap && this.config.webmap) {
                             this.config.webmap = response.itemData.values.webmap;
@@ -290,9 +302,9 @@ define([
         _queryOrganizationInformation: function () {
             var deferred = new Deferred();
             if (this.config.queryForOrg) {
-                // Query the ArcGIS.com organization. This is defined by the sharinghost that is specified. For example if you 
-                // are a member of an org you'll want to set the sharinghost to be http://<your org name>.arcgis.com. We query 
-                // the organization by making a self request to the org url which returns details specific to that organization. 
+                // Query the ArcGIS.com organization. This is defined by the sharinghost that is specified. For example if you
+                // are a member of an org you'll want to set the sharinghost to be http://<your org name>.arcgis.com. We query
+                // the organization by making a self request to the org url which returns details specific to that organization.
                 // Examples of the type of information returned are custom roles, units settings, helper services and more.
                 // If this fails, the application will continue to function
                 esriRequest({
@@ -306,7 +318,7 @@ define([
                     this.orgConfig.units = "metric";
                     if (response.user && response.user.units) { //user defined units
                         this.orgConfig.units = response.user.units;
-                    } else if (response.units) { //org level units 
+                    } else if (response.units) { //org level units
                         this.orgConfig.units = response.units;
                     } else if ((response.user && response.user.region && response.user.region === "US") || (response.user && !response.user.region && response.region === "US") || (response.user && !response.user.region && !response.region) || (!response.user && response.ipCntryCode === "US") || (!response.user && !response.ipCntryCode && kernel.locale === "en-us")) {
                         // use feet/miles only for the US and if nothing is set for a user
@@ -314,7 +326,7 @@ define([
                     }
                     // Get the helper servcies (routing, print, locator etc)
                     this.orgConfig.helperServices = response.helperServices;
-                    // are any custom roles defined in the organization? 
+                    // are any custom roles defined in the organization?
                     if (response.user && esriLang.isDefined(response.user.roleId)) {
                         if (response.user.privileges) {
                             this.orgConfig.userPrivileges = response.user.privileges;
@@ -323,7 +335,7 @@ define([
                     deferred.resolve(true);
                 }), function (error) {
                     if (!error) {
-                        error = new Error("ApplicationBoilerplate:: Error retreiving organization information.");
+                        error = new Error("ApplicationBoilerplate:: Error retrieving organization information.");
                     }
                     deferred.reject(error);
                 });
@@ -333,13 +345,13 @@ define([
             return deferred.promise;
         },
         _queryUrlParams: function () {
-            // This function demonstrates how to handle additional custom url parameters. For example 
-            // if you want users to be able to specify lat/lon coordinates that define the map's center or 
-            // specify an alternate basemap via a url parameter. 
-            // If these options are also configurable these updates need to be added after any 
-            // application default and configuration info has been applied. Currently these values 
-            // (center, basemap, theme) are only here as examples and can be removed if you don't plan on 
-            // supporting additional url parameters in your application. 
+            // This function demonstrates how to handle additional custom url parameters. For example
+            // if you want users to be able to specify lat/lon coordinates that define the map's center or
+            // specify an alternate basemap via a url parameter.
+            // If these options are also configurable these updates need to be added after any
+            // application default and configuration info has been applied. Currently these values
+            // (center, basemap, theme) are only here as examples and can be removed if you don't plan on
+            // supporting additional url parameters in your application.
             this.customUrlConfig = this._createUrlParamsObject(this.config.urlItems);
         }
     });
