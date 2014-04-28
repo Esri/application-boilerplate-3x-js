4954594560c66a90352d8f47fb04f00a9e961acd
MikeTschudi <mtschudi@esri.com>
2014-04-28 09:22:07 -0700

    Added copyright statement and JSLinted

--- application_boilerplate/config/defaults.js
+++ application_boilerplate/config/defaults.js
@@ -1,4 +1,20 @@
-/*global define,location */
+﻿/*global define,location */
+/*jslint sloppy:true */
+/*
+ | Copyright 2014 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
 define({
     //Default configuration settings for the application. This is where you'll define things like a bing maps key,
     //default web map, default app color theme and more. These values can be overwritten by template configuration settings and url parameters.
@@ -21,7 +37,7 @@ define({
     //want to use custom settings for units or any of the helper services set queryForOrg to false then enter
     //default values for any items you need using the helper services and units properties.
     "queryForOrg": true,
-    //If you need localization set the supportsLocalization value to true to get the localized strings
+    //If you need localization set the localize value to true to get the localized strings
     //from the javascript/nls/resource files.
     //Note that we've included a placeholder nls folder and a resource file with one error string
     //to show how to setup the strings file.
