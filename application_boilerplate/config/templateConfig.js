c5f06c06cb76be66fa4ee9074e3eb337aeaa52bd
Matt Driscoll <MDriscoll@esri.com>
2015-02-05 11:38:47 -0800

    #48

--- application_boilerplate/config/templateConfig.js
+++ application_boilerplate/config/templateConfig.js
@@ -45,6 +45,6 @@ define({
   "urlItems": [
     "theme" // example param. ?theme=<my theme>
   ],
-  // For esri hosted environments only. Will automatically create a sharingurl and proxyurl for the application.
+  // Most users will not need to modify this value. For esri hosting environments only. Will automatically create a "sharinghost" and "proxyurl" for the application. Only set this is to true if the app is going to be stored on Esri's hosting servers. If you are using your own custom hosted portal, set the "sharinghost" in defaults.js instead of setting this to true.
   esriEnvironment: false
 });
