f20a177737d386b3428eef68bb9853c3209b6700
Matt Driscoll <MDriscoll@esri.com>
2017-04-17 16:34:34 -0700

    rename final.

---
+++ dist/application/entryPoint.js
@@ -0,0 +1,11 @@
+define(["require", "exports", "application/Application", "boilerplate/Boilerplate"], function (require, exports, Application_1, Boilerplate_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function init(applicationConfigJSON, boilerplateConfigJSON) {
+        var application = new Application_1.default();
+        var boilerplate = new Boilerplate_1.default(JSON.parse(applicationConfigJSON), JSON.parse(boilerplateConfigJSON));
+        boilerplate.load().then(function (response) { return application.init(response); });
+    }
+    exports.init = init;
+});
+//# sourceMappingURL=entryPoint.js.map
