0c297d9ba9421b2f8d538c1078bf9fd55b3f7cce
Matt Driscoll <MDriscoll@esri.com>
2017-04-18 16:23:56 -0700

    cleanup. add resource

---
+++ resources/configurationPanel.js
@@ -0,0 +1,17 @@
+{
+  "configurationSettings": [{
+    "category": "<b>Configure template</b>",
+    "fields": [{
+      "type": "webscene"
+    }, {
+      "type": "string",
+      "fieldName": "title",
+      "label": "Application Title",
+      "tooltip": "Application Title",
+      "placeHolder": "My Application"
+    }]
+  }],
+  "values": {
+    "title": ""
+  }
+}
